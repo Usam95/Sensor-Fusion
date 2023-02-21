82d1b50b0d47608986826a0ea358cead079297c5
Usam Sersultanov <usam.sersultanov@efs-auto.de>
2023-02-21 16:31:27 +0100

    added show_pcl method

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -38,15 +38,29 @@ def show_pcl(pcl):
     print("student task ID_S1_EX2")
 
     # step 1 : initialize open3d with key callback and create window
+    vis_lpc = o3d.visualization.VisualizerWithKeyCallback()
+    vis_lpc.create_window(window_name='Open3D', width=1280, height=1080, left=50, top=50, visible=True)
 
+    global idx
+    idx = True
+
+    def right_click(vis_lpc):
+        global idx
+        print('right arrow pressed')
+        idx = False
+        return
+
+    vis_lpc.register_key_callback(262, right_click)
     # step 2 : create instance of open3d point-cloud class
-
+    pcd = o3d.geometry.PointCloud()
     # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
-
+    pcd.points = o3d.utility.Vector3dVector(pcl[:,:3])
     # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
-    
+    vis_lpc.add_geometry(pcd)
     # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
-
+    while idx:
+        vis_lpc.poll_events()
+        vis_lpc.update_renderer()
     #######
     ####### ID_S1_EX2 END #######     
        
